82227bcbd756a4efb9c928fc59eb0138d0945367
Filip Mamcarczyk <filip.mamcarczyk@codewise.com>
2017-06-02 01:05:42 +0200

    Compressed images

--- src/app/services/photos.service.ts
+++ src/app/services/photos.service.ts
@@ -4,15 +4,15 @@ import { Injectable } from '@angular/core';
 export class PhotosService {
 
   private photos: string[] = [
-    "../../assets/imgs/1.JPG",
-    "../../assets/imgs/2.JPG",
-    "../../assets/imgs/3.JPG",
-    "../../assets/imgs/4.JPG",
-    "../../assets/imgs/5.JPG",
-    "../../assets/imgs/6.JPG",
-    "../../assets/imgs/7.JPG",
-    "../../assets/imgs/8.JPG",
-    "../../assets/imgs/9.JPG"
+    "../../assets/imgs/1.jpg",
+    "../../assets/imgs/2.jpg",
+    "../../assets/imgs/3.jpg",
+    "../../assets/imgs/4.jpg",
+    "../../assets/imgs/5.jpg",
+    "../../assets/imgs/6.jpg",
+    "../../assets/imgs/7.jpg",
+    "../../assets/imgs/8.jpg",
+    "../../assets/imgs/9.jpg"
   ];
 
   constructor() { }
